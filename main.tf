ab4e0aabfa5a912f97c3482f2d569222a842c253
malleswari <maddikirimalleswari4@gmail.com>
2025-11-12 15:10:09 +0530

    message

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "main" {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    prefix_list_ids = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(
